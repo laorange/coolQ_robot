85c0b7c1556f07645b9fc6b35a48b8de4dd4641d
paul-6666 <2625835752@qq.com>
2020-07-19 18:12:29 +0800

    2007191812

--- 酷q小机器人/verify_time.py
+++ 酷q小机器人/verify_time.py
@@ -1,6 +1,7 @@
 import os
 import time
 import sched
+from send import send
 
 os.environ['TZ'] = 'Asia/Shanghai'
 # time.tzset() #Python time tzset() 根据环境变量TZ重新初始化时间相关设置。
@@ -15,5 +16,12 @@ t_weekday = time.strftime("%A", time.localtime())
 s = sched.scheduler(time.time, time.sleep)
 
 
-def verify_time(time2verify):
-    pass
+def verify_time(time2verify_ls):
+    for time2verify in time2verify_ls:
+        t_HM_now = time.strftime("%H%M", time.localtime())
+        if t_HM_now == time2verify.time:
+            send(time2verify.qq, time2verify.message)
+
+
+def every_min_check():
+    s.enter(60, 0, verify_time, )
